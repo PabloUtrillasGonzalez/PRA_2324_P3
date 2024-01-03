c35b1e232c271a22e2ce86429395929d4ae6153f
Pablo Utrillas Gonzalez <putrgon@upv.edu.es>
2024-01-03 14:06:40 +0100

    Añadidos comentarios

--- BSTreeDict.h
+++ BSTreeDict.h
@@ -8,44 +8,44 @@
 #include "TableEntry.h"
 
 template <typename V>
-class BSTreeDict: public Dict<V> {
+class BSTreeDict: public Dict<V> {	// Clase intermediaria entre BSTree y Dict
 
     private:
 	BSTree<TableEntry<V>>* tree;
 
     public:
-	BSTreeDict(){
+	BSTreeDict(){ // Tree vacio
 		tree = new BSTree<TableEntry<V>>;
 	}
 
-	~BSTreeDict(){
+	~BSTreeDict(){	// Borramos dinamica
 		delete tree;
 	}
 
-	friend ostream& operator<<(ostream &out, const BSTreeDict<V> &bs){
-		return out << *bs.tree << endl;
+	friend ostream& operator<<(ostream &out, const BSTreeDict<V> &bs){ // Print
+		return out << *bs.tree;
 	}
 
-	V operator[](string key){
+	V operator[](string key){	// Devolvemos valor de la key
 		return tree->search(TableEntry<V>(key)).value;
 	}
 
 	// Implementación metodos abstractos clase Dict<V>
-	void insert(string key, V value){
+	void insert(string key, V value){	//Insert clave, valor
 		tree->insert(TableEntry<V>(key,value));
 	}
 
-	V search(string key){
+	V search(string key){	//Busqueda del valor teniendo clave
 		return tree->search(TableEntry<V>(key)).value;	
 	}
 
-	V remove(string key){
+	V remove(string key){	// Eliminamos mediante clave y devolvemos el valor que tenia
 		V valor = tree->search(TableEntry<V>(key)).value;
 		tree->remove(TableEntry<V>(key));
 		return valor;
 	}
 
-	int entries() const override {
+	int entries() const override {	// Implementamos metodo entries
         	return tree->size();
 	}
 
